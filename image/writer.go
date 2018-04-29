2461e2093a66527ce02a951190e7b7f8c3a55665
daiiz <da.iz.mg@gmail.com>
2018-04-29 13:24:07 +0900

    bmp形式で出力

--- image/writer.go
+++ image/writer.go
@@ -4,10 +4,18 @@ import (
 	"image"
 	"image/png"
 	"os"
+
+	"golang.org/x/image/bmp"
 )
 
 func saveAsPng(img image.Image, path string) {
 	file, _ := os.Create(path)
 	defer file.Close()
 	png.Encode(file, img)
+}
+
+func saveAsBmp(img image.Image, path string) {
+	file, _ := os.Create(path)
+	defer file.Close()
+	bmp.Encode(file, img)
 }
