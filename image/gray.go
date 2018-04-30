df911648e6e34741b2d90522e22acc0949d20d80
daiiz <da.iz.mg@gmail.com>
2018-04-30 17:30:06 +0900

    グレー化できた

--- image/gray.go
+++ image/gray.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"image"
-	// "image/color"
+	"image/color"
 	// "image/png"
 )
 
 func toGray(img image.Image) image.Image {
-	return img
+	bounds := img.Bounds()
+	dest := image.NewGray(bounds)
+	for y := 0; y < bounds.Max.Y; y++ {
+		for x := 0; x < bounds.Max.X; x++ {
+			c := color.GrayModel.Convert(img.At(x, y))
+			gray, _ := c.(color.Gray)
+			dest.Set(x, y, gray)
+		}
+	}
+	return dest
 }
